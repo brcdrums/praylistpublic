0f05f703b3eec09bf9cf35c14a379e40795c8a41
brcdrums <bryan@lark.io>
2013-02-28 02:00:12 -0600

    styled dropdown menu more but still can't figure out highlighting. needs a lot of work still

--- PrayList/static/new.js
+++ PrayList/static/new.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('#youpray').hide().fadeIn(1000);
+    $("#timedropdownul a").hide();
+    $('.timedropselected').click(function(){
+        $('#timedropdownul a').toggle();
+
+    });
+
+    
   });
