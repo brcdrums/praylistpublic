7b0d4b54674f08eb871cb0fc1f429e0e2deb5a0f
brcdrums <bryan@lark.io>
2013-03-06 02:16:07 -0600

    added skeleton for first test case

---
+++ PrayList_testsuite/New_Timestamps.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+import TestBase
+
+class NewTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_title(self):
+        self.driver.get(TestBase.newURL)
+        self.assertTrue("new" in self.driver.title)
+
+    def suite(self):
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(NewTest))
+        return suite
+
+if __name__ == '__main__':
+   unittest.main()
---
+++ PrayList_testsuite/TestBase.py
@@ -0,0 +1,10 @@
+# *---------Global Variables---------*
+# URLs
+homeURL = "http://127.0.0.1:8000/"
+newURL = "http://127.0.0.1:8000/new/"
+top_today_URL = "http://127.0.0.1:8000/top/"
+top_week_URL = "http://127.0.0.1:8000/top/week/"
+top_month_URL = "http://127.0.0.1:8000/top/month/"
+top_year_URL = "http://127.0.0.1:8000/top/year/"
+top_all_URL = "http://127.0.0.1:8000/top/all/"
+
