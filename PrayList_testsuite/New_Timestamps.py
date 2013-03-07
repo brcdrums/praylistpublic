26881964e416c43bafe43ae1287c631108165ee2
brcdrums <bryan@lark.io>
2013-03-07 00:23:40 -0600

    added first real test case which makes sure timestamps are in correct order on new page

--- PrayList_testsuite/New_Timestamps.py
+++ PrayList_testsuite/New_Timestamps.py
@@ -13,10 +13,30 @@ class NewTest(unittest.TestCase):
         self.driver.get(TestBase.newURL)
         self.assertTrue("new" in self.driver.title)
 
+    def test_timestamp(self):
+        self.driver.get(TestBase.newURL)
+        timestamps = self.driver.find_elements_by_class_name("timestamp")
+        print "length of timestamps = " + str(len(timestamps))
+        for stamp, next in timestampgen(timestamps):
+            current_item = stamp.text
+            next_item = next.text
+            print "current item = " + current_item + "next item = " + next_item
+            current_item_cleaned = TestBase.equate_time(current_item)
+            next_item_cleaned = TestBase.equate_time(next_item)
+            self.assertTrue(current_item_cleaned <= next_item_cleaned)
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(NewTest))
     return suite
+
+def timestampgen(timestamps):
+    timestamp_iter = iter(timestamps)
+    current = timestamp_iter.next()
+    for next in timestamp_iter:
+        yield current, next
+        current = next
 
 if __name__ == '__main__':
    unittest.main()
--- PrayList_testsuite/TestBase.py
+++ PrayList_testsuite/TestBase.py
@@ -1,3 +1,5 @@
+import re
+
 # *---------Global Variables---------*
 # URLs
 homeURL = "http://127.0.0.1:8000/"
@@ -8,3 +10,17 @@ top_month_URL = "http://127.0.0.1:8000/top/month/"
 top_year_URL = "http://127.0.0.1:8000/top/year/"
 top_all_URL = "http://127.0.0.1:8000/top/all/"
 
+# Functions
+def equate_time(item):
+    if("hour" in item):
+        intval = re.sub("[a-zA-Z ]", "", item).strip()
+        intval_clean = int(intval)
+        return intval_clean
+    elif("day" in item):
+        intval = re.sub("[a-zA-Z ]", "", item).strip()
+        intval_clean = int(intval) * 24
+        return intval_clean
+    elif("week" in item):
+        intval = re.sub("[a-zA-Z ]", "", item).strip()
+        intval_clean = int(intval) * 24 * 7
+        return intval_clean
