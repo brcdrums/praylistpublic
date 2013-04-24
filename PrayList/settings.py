61e5b80b7fea6692ac2d8a61e802222b28deeff4
Matt Schultz <szidev@gmail.com>
2013-04-23 20:49:27 -0500

    Fixed static directory pathing.

--- PrayList/settings.py
+++ PrayList/settings.py
@@ -1,3 +1,5 @@
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -54,7 +56,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/thebirdthebear/Documents/workspace/PrayList/PrayList/PrayList/static/'
+# STATIC_ROOT = '/Users/thebirdthebear/Documents/workspace/PrayList/PrayList/PrayList/static/'
+STATIC_ROOT = os.path.join(os.getcwd(), 'PrayList/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -65,7 +68,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '~/Users/thebirdthebear/Documents/workspace/PrayList/PrayList/PrayList/static',
+    # '~/Users/thebirdthebear/Documents/workspace/PrayList/PrayList/PrayList/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -108,7 +111,8 @@ ROOT_URLCONF = 'PrayList.urls'
 WSGI_APPLICATION = 'PrayList.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/thebirdthebear/Documents/workspace/PrayList/PrayList/PrayList',
+    # '/Users/thebirdthebear/Documents/workspace/PrayList/PrayList/PrayList',
+    os.path.join(os.getcwd(), 'PrayList'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
